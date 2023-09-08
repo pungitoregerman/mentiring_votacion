ad0ee7bf12f6b5007bd265132fcf08060bd67cc7
pungitoregerman <pungitoregerman@gmail.com>
2023-09-08 17:01:00 -0300

    modificacion de estilos

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import React, { useState, useEffect } from "react";
 import { doc, setDoc, updateDoc, getDoc, onSnapshot } from "firebase/firestore";
 import db from "./firebase"; // Importa la referencia a Firestore desde tu archivo firebase.js
 import "./App.css";
+import Swal from 'sweetalert2'
+
 
 const options = [
   {
     id: "option1",
-    imageSrc: "https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/263a.png", // Ruta a la imagen (reemplaza con tu ruta)
+    imageSrc: "./img/enjoys.png",
   },
   {
     id: "option2",
-    imageSrc: "https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f60d.png", // Ruta a la imagen (reemplaza con tu ruta)
+    imageSrc: "./img/enj.png", 
   },
   {
     id: "option3",
-    imageSrc: "https://as01.epimg.net/epik/imagenes/2018/11/05/portada/1541440062_346544_1541440238_noticia_normal_recorte1.jpg", // Ruta a la imagen (reemplaza con tu ruta)
+    imageSrc: "./img/lents.png", 
   },
   {
     id: "option4",
-    imageSrc: "https://ep01.epimg.net/verne/imagenes/2017/07/26/articulo/1501057501_658002_1501059072_noticia_normal.jpg", // Ruta a la imagen (reemplaza con tu ruta)
+    imageSrc: "./img/silent.png", 
   },
 ];
 
+const optionColors = ["#06507a", "#078f3b", "#470466", "#af0505"];
+
 function App() {
   const [votes, setVotes] = useState({});
+  const [hasVoted, setHasVoted] = useState(false);
 
   useEffect(() => {
     const votosDocRef = doc(db, "votos", "votos");
@@ -37,6 +42,12 @@ function App() {
       }
     });
 
+    // Verificar si el usuario ha votado anteriormente
+    const hasVotedPreviously = localStorage.getItem("hasVoted");
+    if (hasVotedPreviously) {
+      setHasVoted(true);
+    }
+
     return () => {
       // Detener la escucha cuando el componente se desmonta
       unsubscribe();
@@ -44,6 +55,15 @@ function App() {
   }, []);
 
   const handleVote = async (optionId) => {
+    if (hasVoted) {
+      Swal.fire(
+        'Ya votaste, no podes votar de nuevo!',
+        '',
+        'warning'
+      )
+      return;
+    }
+
     try {
       const votosDocRef = doc(db, "votos", "votos");
 
@@ -54,6 +74,10 @@ function App() {
         // El documento existe, actualízalo
         const updatedVotes = { ...votes, [optionId]: (votes[optionId] || 0) + 1 };
         await updateDoc(votosDocRef, updatedVotes);
+
+        // Marcar al usuario como que ya ha votado
+        localStorage.setItem("hasVoted", "true");
+        setHasVoted(true);
       } else {
         // El documento no existe, créalo con valores iniciales
         const initialVotes = {};
@@ -61,33 +85,46 @@ function App() {
           initialVotes[opt.id] = opt.id === optionId ? 1 : 0;
         });
         await setDoc(votosDocRef, initialVotes);
+        // Marcar al usuario como que ya ha votado
+        localStorage.setItem("hasVoted", "true");
+        setHasVoted(true);
       }
     } catch (error) {
       console.error("Error al actualizar los votos:", error);
     }
-  };
 
+    Swal.fire(
+      'Gracias por tu voto!',
+      '',
+      'success'
+    )
+  };
+  
   return (
     <div className="App" style={{
-      backgroundImage: `url('')`,
-      backgroundSize: 'cover', // Cubrir todo el fondo
-      backgroundRepeat: 'no-repeat', // Evitar la repetición de la imagen
-      backgroundAttachment: 'fixed', // Fijar la imagen de fondo
-      backgroundPosition: 'center center', // Centrar la imagen horizontal y verticalmente
+      backgroundImage: `url('https://img.freepik.com/vector-gratis/fondo-abstracto-acuarela_220290-24.jpg')`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat', 
+      backgroundAttachment: 'fixed', 
+      backgroundPosition: 'center center',
     }}>
       <div className="logos-container">
         <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQzPxHmR7v9kjmUqWVz_fFH0WL46T7qvvuxqGe8kpCbX00STpjHCp-b5R7vRw8QhlfgL8&usqp=CAU" alt="Logo 1" className="logo" />
         <img  src="https://i0.wp.com/www.pescar.org.ar/wp-content/uploads/2021/10/Home-Boceto-Pescar-2021-312x96-1.png?resize=312%2C96&ssl=1" alt="Logo 2" className="logo" />
       </div>
       <h1>¿Podes definir el encuentro con un Emoji?</h1>
-      <h3>Elegí cual te represento: </h3>
+      <h3>Clickea cual te represento: </h3>
       <div className="options">
         <div className="options-container">
-          {options.map((option) => (
+          {options.map((option,index) => (
             <div className="option" key={option.id}>
               <button
                 className="image-button"
                 onClick={() => handleVote(option.id)}
+                style={{
+                  backgroundColor: optionColors[index], 
+                borderRadius: '30px',
+                }}
               >
                 <img className='emoji-vote' src={option.imageSrc} alt={`Imagen de ${option.id}`} />
               </button>
@@ -98,7 +135,7 @@ function App() {
       <div className="div-votes">
       {options.map((option) => (
             <div className="option" key={option.id}>
-              <span><img className='emoji-vote' src={option.imageSrc} alt={`Img votes ${option.id}`}/> {votes[option.id] || 0}</span>
+              <span><img className='emoji-vote' src={option.imageSrc} alt={`Img votes ${option.id}`}/> <p className="cantidad-votos">{votes[option.id] || 0}</p></span>
             </div>
           ))}
       </div>
